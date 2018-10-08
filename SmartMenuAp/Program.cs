9d041208fce950296bcf274b56b457d4c74bb862
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-10-08 13:15:23 +0200

    Make errormessages language independant.

--- SmartMenuAp/Program.cs
+++ SmartMenuAp/Program.cs
@@ -19,8 +19,41 @@ namespace SmartMenuAp
         {
             SmartMenu menu = new SmartMenu();
             Bindings bindings = new Bindings();
-            menu.LoadMenu("MenuSpec.txt");
+
+            //TODO: Be able to choose language.
+            string language, errorPath;
+            language = ChooseLanguage(out errorPath);
+            menu.LoadMenu(language, errorPath);
             menu.Activate(bindings);
+        }
+
+        private string ChooseLanguage(out string errorFile)
+        {
+            int userInput = 0;
+            string menuChoice;
+
+            Console.WriteLine("Press 1 for English");
+            Console.WriteLine("Tryk 2 for dansk");
+
+            while (!int.TryParse(Console.ReadLine(), out userInput) || userInput > 2 || userInput < 1)
+            {
+                Console.WriteLine("Please enter valid input.");
+                Console.WriteLine("Indtast veligst en gyldig værdi.");
+            }
+
+            if (userInput == 1)
+            {
+                menuChoice = "MenuSpecEn.txt";
+                errorFile = "ErrorlistEn.txt";
+            }
+            else
+            {
+                menuChoice = "MenuSpecDk.txt";
+                errorFile = "ErrorlistDk.txt";
+            }
+
+            return menuChoice;
+
         }
     }
 }
