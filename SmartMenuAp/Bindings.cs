c186a624d43e03cfc48af2a454596d1a8c3dea21
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-10-08 12:26:24 +0200

    Create solution.

---
+++ SmartMenuAp/Bindings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartMenuLibrary;
+using FunctionLibrary;
+
+namespace SmartMenuAp
+{
+    class Bindings : IBindings
+    {
+        public void Call(string callId)
+        {
+            switch (callId)
+            {
+                case "linie1":
+                    Console.WriteLine(Functions.DoThis());
+                    break;
+                case "linie2":
+                    Console.WriteLine(Functions.DoThat());
+                    break;
+                case "linie3":
+                    Console.WriteLine("Tell me what to do.");
+                    string userInput = Console.ReadLine();
+                    Console.WriteLine(Functions.DoSomething(userInput));
+                    break;
+                case "linie4":
+                    Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                    break;
+                default:
+                    Console.WriteLine("This is the default switch");
+                    break;
+            }
+        }
+    }
+}
--- SmartMenuAp/Program.cs
+++ SmartMenuAp/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FunctionLibrary;
+using SmartMenuLibrary;
 
 namespace SmartMenuAp
 {
@@ -10,12 +12,15 @@ namespace SmartMenuAp
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Program myProgram = new Program();
+            myProgram.Run();
+        }
+        private void Run()
+        {
+            SmartMenu menu = new SmartMenu();
+            Bindings bindings = new Bindings();
+            menu.LoadMenu("MenuSpec.txt");
+            menu.Activate(bindings);
         }
     }
 }
