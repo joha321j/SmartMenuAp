0d927d65f4350f9896eff073e7888c3d427a1e3f
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2018-10-10 10:38:45 +0200

    Add DCD and remove ChooseLanguage method.

--- SmartMenuAp/Program.cs
+++ SmartMenuAp/Program.cs
@@ -22,38 +22,9 @@ namespace SmartMenuAp
 
             //TODO: Be able to choose language.
             string language, errorPath;
-            language = ChooseLanguage(out errorPath);
+            language = menu.ChooseLanguage(out errorPath);
             menu.LoadMenu(language, errorPath);
             menu.Activate(bindings);
-        }
-
-        private string ChooseLanguage(out string errorFile)
-        {
-            int userInput = 0;
-            string menuChoice;
-
-            Console.WriteLine("Press 1 for English");
-            Console.WriteLine("Tryk 2 for dansk");
-
-            while (!int.TryParse(Console.ReadLine(), out userInput) || userInput > 2 || userInput < 1)
-            {
-                Console.WriteLine("Please enter valid input.");
-                Console.WriteLine("Indtast veligst en gyldig værdi.");
-            }
-
-            if (userInput == 1)
-            {
-                menuChoice = "MenuSpecEn.txt";
-                errorFile = "ErrorlistEn.txt";
-            }
-            else
-            {
-                menuChoice = "MenuSpecDk.txt";
-                errorFile = "ErrorlistDk.txt";
-            }
-
-            return menuChoice;
-
         }
     }
 }
